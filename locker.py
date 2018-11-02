d0fec4e25b911f0b7e3812649397933d80aab79f
Kanogaelias <kanogae@gmail.com>
2018-11-02 14:55:18 +0300

    add method to show if username and password are correct

--- locker.py
+++ locker.py
@@ -4,7 +4,7 @@ class Credentials:
     Class that creates accounts and authenticates the users
     '''
     users_list=[]
-    
+
     def __init__(self, identify, user_name, password):
         '''
         Initalizing the variables
@@ -12,3 +12,20 @@ class Credentials:
         self.identify = identify
         self.user_name = user_name
         self.password = password
+
+            def create_account(self):
+        '''
+        creating and saving log in credentials for the various users
+        '''
+        Credentials.users_list.append(self)
+    
+    @classmethod
+    def authenticate_account(cls, name, key):
+        '''
+        Method that checks if the username and password are correct
+        '''
+        for user in cls.users_list:
+            if user.user_name == name and user.password == key:
+                # print(user.identify)
+                return user
+        return 0
