ada8b12ea33316bc9f2757066dd0eed0668a326b
Kanogaelias <kanogae@gmail.com>
2018-11-02 15:34:00 +0300

    creates function to store passwords,to save new data created and to display the data

--- run.py
+++ run.py
@@ -20,3 +20,23 @@ def authenticate(username,passkey):
     Function responsible for signing in
     '''
     return Credentials.authenticate_account(username,passkey)
+
+
+def my_new_data(user_id,data_id,website,web_key):
+    '''
+    Function that creates new data for storing password
+    '''
+    new_data = UsersData(user_id,data_id,website,web_key)
+    return new_data
+
+def add_data(data):
+    '''
+    Function that saves the new data created
+    '''
+    data.add_password()
+
+def display_data(data,number):
+    '''
+    Function that displays the user data
+    '''
+    return UsersData.display_data(data,number)
