2ed400be771eff1dc6cf13c616825f7b47b0014f
Kanogaelias <kanogae@gmail.com>
2018-11-02 15:01:23 +0300

    test case to see if it has been initialized properly

--- locker_test.py
+++ locker_test.py
@@ -10,4 +10,17 @@ class TestCredentials(unittest.TestCase):
         Setting up the structure before each test
         '''
         self.new_user = Credentials(1,"richie","uiui")
+        
+    def tearDown(self):
+        '''
+        Cleans up after each test has run
+        '''
+        Credentials.users_list = []
     
+    def test_init(self):
+        '''
+        Test case to test if the case has been initialized properly
+        '''
+        self.assertEqual(self.new_user.identify,1)
+        self.assertEqual(self.new_user.user_name,"richie")
+        self.assertEqual(self.new_user.password,"uiui")
